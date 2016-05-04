a58cca7503e17d99e65a09b6341af77f204ed623
Luis Felipe Gil <lamaslg@gmail.com>
2016-05-04 10:15:05 +0200

    a

---
+++ Modelos/Alumnos/AlumnoService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modelos.Alumnos
+{
+    /// <summary>
+    /// All the methods for retrieving, updating and deleting data are implemented in this class file.
+    /// The samples below show the finder and specific finder method for Entity1.
+    /// </summary>
+    public class AlumnoService
+    {
+        /// <summary>
+        /// This is a sample specific finder method for Entity1.
+        /// If you want to delete or rename the method think about changing the xml in the BDC model file as well.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Entity1</returns>
+        public static Alumno ReadItem(int id)
+        {
+            //TODO: This is just a sample. Replace this simple sample with valid code.
+            Alumno al = new Alumno();
+            
+            return al;
+        }
+        /// <summary>
+        /// This is a sample finder method for Entity1.
+        /// If you want to delete or rename the method think about changing the xml in the BDC model file as well.
+        /// </summary>
+        /// <returns>IEnumerable of Entities</returns>
+        public static IEnumerable<Alumno> ReadList()
+        {
+            //TODO: This is just a sample. Replace this simple sample with valid code.
+            Alumno[] entityList = new Alumno[1];
+           
+            return entityList;
+        }
+
+       
+
+        
+    }
+}
---
+++ Modelos/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="e44e939a-be6c-4daf-9a37-4ed968c30559" featureId="e44e939a-be6c-4daf-9a37-4ed968c30559" imageUrl="" scope="Farm" solutionId="00000000-0000-0000-0000-000000000000" title="Modelos Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="4dc64fec-188b-4fb5-81bf-c68d686a8eea" />
+  </projectItems>
+</feature>
